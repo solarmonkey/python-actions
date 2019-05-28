5c0ddc17d8955e2bc283359fccb8321faed7de7d
Tino de Bruijn <tino@solarmonkey.nl>
2019-05-28 11:55:02 +0200

    No need for a venv?

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-VENV_NAME=${VENV_DIR:=venv}
+# VENV_NAME=${VENV_DIR:=venv}
 
-if ! [ -e "${GITHUB_WORKSPACE}/${VENV_NAME}" ]; then
-    python -m venv "${GITHUB_WORKSPACE}/${VENV_NAME}"
-fi
+# if ! [ -e "${GITHUB_WORKSPACE}/${VENV_NAME}" ]; then
+#     python -m venv "${GITHUB_WORKSPACE}/${VENV_NAME}"
+# fi
 
-source "${GITHUB_WORKSPACE}/${VENV_NAME}/bin/activate"
+# source "${GITHUB_WORKSPACE}/${VENV_NAME}/bin/activate"
 
 sh -c "$*"
